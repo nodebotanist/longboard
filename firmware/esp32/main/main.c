543d563b814667394eb648ea400985c99de0576e
Kas Perch <the@nodebotani.st>
2017-04-24 23:30:48 -0500

    ready for req library

--- firmware/esp32/main/main.c
+++ firmware/esp32/main/main.c
@@ -3,12 +3,206 @@
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_event_loop.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/adc.h"
+#include "ws2812.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include <string.h>
+#include <stdlib.h>
+
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+
+#define WS2812_PIN	18
+
+#define delay_ms(ms) vTaskDelay((ms) / portTICK_RATE_MS)
+
+const static char http_html_hdr[] =
+    "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+const static char http_index_hml[] = "<!DOCTYPE html>"
+      "<html>\n"
+      "<head>\n"
+      "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+      "  <style type=\"text/css\">\n"
+      "    html, body, iframe { margin: 0; padding: 0; height: 100%; }\n"
+      "    iframe { display: block; width: 100%; border: none; }\n"
+      "  </style>\n"
+      "<title>HELLO ESP32</title>\n"
+      "</head>\n"
+      "<body>\n"
+      "<h1>Hello World, from ESP32!</h1>\n"
+      "</body>\n"
+      "</html>\n";
+
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+
+void rainbow(void *pvParameters)
+{
+  const uint8_t anim_step = 10;
+  const uint8_t anim_max = 250;
+  const uint8_t pixel_count = 51; // Number of your "pixels"
+  const uint8_t delay = 25; // duration between color changes
+  rgbVal color = makeRGBVal(anim_max, 0, 0);
+  uint8_t step = 0;
+  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
+  uint8_t step2 = 0;
+  rgbVal *pixels;
+
+
+  pixels = malloc(sizeof(rgbVal) * pixel_count);
+
+  while (1) {
+    color = color2;
+    step = step2;
+
+    for (uint8_t i = 0; i < pixel_count; i++) {
+      pixels[i] = color;
+
+      if (i == 1) {
+        color2 = color;
+        step2 = step;
+      }
+
+      switch (step) {
+      case 0:
+        color.g += anim_step;
+        if (color.g >= anim_max)
+          step++;
+        break;
+      case 1:
+        color.r -= anim_step;
+        if (color.r == 0)
+          step++;
+        break;
+      case 2:
+        color.b += anim_step;
+        if (color.b >= anim_max)
+          step++;
+        break;
+      case 3:
+        color.g -= anim_step;
+        if (color.g == 0)
+          step++;
+        break;
+      case 4:
+        color.r += anim_step;
+        if (color.r >= anim_max)
+          step++;
+        break;
+      case 5:
+        color.b -= anim_step;
+        if (color.b == 0)
+          step = 0;
+        break;
+      }
+    }
+
+    ws2812_setColors(pixel_count, pixels);
+
+    delay_ms(delay);
+  }
+}
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-    return ESP_OK;
+   if (event->event_id == SYSTEM_EVENT_STA_GOT_IP) {
+      printf("Our IP address is " IPSTR "\n",
+         IP2STR(&event->event_info.got_ip.ip_info.ip));
+      printf("We have now connected to a station and can do things...\n");
+   }
+   switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        //xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        //xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+  }
+   return ESP_OK;
+}
+
+static void http_server_netconn_serve(struct netconn *conn)
+{
+  struct netbuf *inbuf;
+  char *buf;
+  u16_t buflen;
+  err_t err;
+
+  /* Read the data from the port, blocking if nothing yet there.
+   We assume the request (the part we care about) is in one netbuf */
+  err = netconn_recv(conn, &inbuf);
+
+  if (err == ERR_OK) {
+    netbuf_data(inbuf, (void**)&buf, &buflen);
+
+    /* Is this an HTTP GET command? (only check the first 5 chars, since
+    there are other formats for GET, and we're keeping it very simple )*/
+    if (buflen>=5 &&
+        buf[0]=='G' &&
+        buf[1]=='E' &&
+        buf[2]=='T' &&
+        buf[3]==' ' &&
+        buf[4]=='/' ) {
+          printf("%c\n", buf[5]);
+      /* Send the HTML header
+             * subtract 1 from the size, since we dont send the \0 in the string
+             * NETCONN_NOCOPY: our data is const static, so no need to copy it
+       */
+      netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+
+      /* Send our HTML page */
+      netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
+    }
+
+  }
+  /* Close the connection (server closes in HTTP) */
+  netconn_close(conn);
+
+  /* Delete the buffer (netconn_recv gives us ownership,
+   so we have to make sure to deallocate the buffer) */
+  netbuf_delete(inbuf);
+}
+
+static void http_server(void *pvParameters)
+{
+  struct netconn *conn, *newconn;
+  err_t err;
+  conn = netconn_new(NETCONN_TCP);
+  netconn_bind(conn, NULL, 80);
+  netconn_listen(conn);
+  do {
+     err = netconn_accept(conn, &newconn);
+     if (err == ERR_OK) {
+       http_server_netconn_serve(newconn);
+       netconn_delete(newconn);
+     }
+   } while(err == ERR_OK);
+   netconn_close(conn);
+   netconn_delete(conn);
 }
 
 void app_main(void)
@@ -22,21 +216,34 @@ void app_main(void)
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     wifi_config_t sta_config = {
         .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
+            .ssid = "Alderaan Shot First",
+            .password = "AceMarshallBatman",
             .bssid_set = false
         }
     };
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
+    adc1_config_width(ADC_WIDTH_12Bit);
+    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_0db);
+
+    ws2812_init(WS2812_PIN);
+    xTaskCreate(rainbow, "ws2812 rainbow demo", 4096, NULL, 10, NULL);
+
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
+
+    int val = 0;
+    int oldval = 0;
+    bool crash = false;
     while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+        oldval = val;
+        val = adc1_get_voltage(ADC1_CHANNEL_0);
+        if(val - oldval < -10){
+          crash = true;
+        } else {
+          crash = false;
+        }
+        printf("Accel readout: %d\n", val);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
-
